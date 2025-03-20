3647e450d5524174f8baef651928fe062a2c055e
saidabj <boujamaaouisaida@gmail.com>
2025-03-20 23:48:20 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import About from './components/About';
-import Contact from './components/Contact';
+//import Header from './components/Header';
+//import Footer from './components/Footer';
+//import About from './components/About';
+//import Contact from './components/Contact';
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CircuitsList from "./components/circuitlist";
+import Autres from "./components/autres";
+import Reserver from "./components/reserver";
+
 function App() {
   return (
     <div>
     
  
 
-      <Header /><br/>
-     {/*  <About />
-       <Contact />*/}
-       <Footer /> 
+      {/*<Header /><br/>
+       <About />*/}
+
+       <Router>
+      <Routes>
+        <Route path="/" element={<CircuitsList />} />
+        <Route path="/activites" element={<Autres />} />
+        <Route path="/trip/:tripId" element={<Reserver />}/>
+      </Routes>
+    </Router>
+       {/*  <Contact /> <Footer />  */}
+       
     </div>
   );
 }
